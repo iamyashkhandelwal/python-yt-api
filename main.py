7dc7754b6d6f377928b5895506721c29cfd9a546
Yash Khandelwal <yashkhandelwal@appscrip.co>
2023-02-06 15:41:32 +0530

    Added CORS settings

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from routes import  downloadVideo, downloadAudioOnly, downloadPlaylistVideos
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
